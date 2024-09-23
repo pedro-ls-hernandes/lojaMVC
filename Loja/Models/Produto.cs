b98078404e12badc3758441c6f20e318dc81d1ea
pedros.hernandes <pedro.s.hernandes845@gmail.com>
2024-09-23 20:25:59 -0300

    first commit

---
+++ Loja/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace Loja.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string? Nome { get; set; }
+        public double Preco { get; set; }
+
+        public int CategoriaId { get; set;}
+        public Categoria? Categoria { get; set; }
+        
+
+    }
+}
